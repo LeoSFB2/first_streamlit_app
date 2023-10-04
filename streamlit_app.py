0f5958bba9ca110ba5b515237a1bad4ed6f71b1a
LeoSFB2 <146800530+LeoSFB2@users.noreply.github.com>
2023-10-04 11:02:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
@@ -7,6 +8,11 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')               
 streamlit.text('🐔 Hard Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
+streamlit.text(' ')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.text(' ')
+streamlit.dataframe(my_fruit_list)
+
